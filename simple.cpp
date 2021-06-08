b65193ec1bdd9d311bf1807490c24980b367cfcd
Габанакгоси Шарон <sofentse-gabanakgosi@neoflex.ru>
2021-06-08 16:15:04 +0200

    first  commit

---
+++ simple.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"'Hello, my name is Sharon"<<endl;
+
+	cout<<endl;
+
+	cout<<"The purpose of this file is just to use to create a docker image"<<endl;
+
+	return 0;
+}
